c378a625eaf7d38386cef7d94da434ba6172608b
cawhitman <cawhit13@gmail.com>
2016-02-29 17:09:20 -0500

    Second iteration

--- p2.c
+++ p2.c
@@ -3,21 +3,29 @@
 
 //Prototypes
 long getFileSize(char *filename);
-int compress();
+char *compress(char *filename, long fileSize);
 int decompress();
 
 //Globals
 FILE *fp;
+long fileSize;
 
 int main()
 {
-	long fileSize;
 
 	fp = stdin;
+	
+	//get filesize of standard in 
 	fileSize = getFileSize(fp);
 
+	//create output buffer
+	char *compressionbuffer = malloc(fileSize + 1);
 
+	//compress 8 bits to 7 bits
+	compressionbuffer = compress(fp, fileSize);
+	
 
+	free(compressionbuffer);
 	return 0;
 }
 
@@ -37,9 +45,22 @@ long getFileSize (char *filename)
 }
 
 //Compresses 8 bit ASCII characters into 7 bits 
-int compress()
+char *compress(char *filename, long fileSize)
 {
+	//create output buffer
+	char *outbuffer = malloc(fileSize + 1);
 
+	int charBuf; 
+	float bytes = filesize / 8; 
+	f = fopen(filename, "rb");
+	int count = 0;
+	while((charBuff = fgetc(filename)!= EOF) && count <= bytes)
+	{
+		outbuffer |= (charBuf << (7 * count));
+		count++; 
+	}
+
+	return outbuffer; 
 }
 
 // 32 bit integer is written at the start of the output file
